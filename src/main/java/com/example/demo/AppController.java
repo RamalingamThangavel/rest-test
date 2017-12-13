725b415b044804bca505cee710135b0522bd851a
Ramalingam Thangavel <ram@miamidade.gov>
2017-12-13 14:07:42 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ram on 12/13/2017
+ */
+@RestController
+@RequestMapping("/api")
+public class AppController {
+
+    @RequestMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Map helloWorld(){
+        Map result = new HashMap();
+        result.put("name", "Ram");
+        return result;
+    }
+}
