6beb4a21b3cc76011d61e50f08bd3fed849d6afc
RamalingamThangavel <RamalingamThangavel@users.noreply.github.com>
2017-12-20 14:01:12 -0500

    Update AppController.java

--- src/main/java/ram/services/rest/AppController.java
+++ src/main/java/ram/services/rest/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
     @RequestMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public Map helloWorld(){
         Map result = new HashMap();
-        result.put("name", "Ram test to be released for staging");
+        result.put("name", "Ram test added new feature");
         return result;
     }
 }
