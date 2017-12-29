3e8eb0af589de53972a44f4312cc8672c17b5827
RamalingamThangavel <RamalingamThangavel@users.noreply.github.com>
2017-12-29 09:38:29 -0500

    Update AppController.java
    release to stage

--- src/main/java/ram/services/rest/AppController.java
+++ src/main/java/ram/services/rest/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
     @RequestMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public Map helloWorld(){
         Map result = new HashMap();
-        result.put("name", "Ram test added new feature");
+        result.put("name", "to be released for staging");
         return result;
     }
 }
